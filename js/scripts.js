43c1732640387b0e32e92a6f65b5f11b03689197
Priscila <prilrosas@gmail.com>
2024-08-12 16:14:10 -0400

    limpando o espaço do código

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,9 @@
 const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
-
 const multiplicationTitle = document.querySelector(
   "#multiplication-title span"
 );
-
 const multiplicationTable = document.querySelector(
   "#multiplication-operations"
 );
